97fcf9763059291c3ed8d64c8f46ca2373c39963
Michael Federczuk <federczuk.michael@hotmail.com>
2019-06-10 16:40:33 +0200

    Renamed output file to 'test' and added .gitignore

--- assemble.sh
+++ assemble.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 # Builds the utest script using spp.
-# Copyright (C) 2019 Michael Federczuk 
-# 
+# Copyright (C) 2019 Michael Federczuk
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
@@ -26,7 +26,7 @@ if ! command -v 'spp' 1>/dev/null; then
 fi
 
 basefile='src/base.sh'
-outfile='test.sh'
+outfile='test'
 
 echo "Building file '$outfile' ..."
 if ! spp "$basefile" 1> "$outfile"; then
